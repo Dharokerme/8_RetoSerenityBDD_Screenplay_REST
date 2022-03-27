551111dd65b10ca6c86f9bdd9a6fd546bd0cd31f
Juan_Esteban_Cardona <juanes.cardona@hotmail.com>
2022-03-27 03:32:11 -0500

    Estrutura Basica: Dependencias Serenity BDD - Log4j Patron Screenplay

---
+++ src/test/java/co/com/sofka/stepdefinitions/setup/BaseResources.java
@@ -0,0 +1,13 @@
+package co.com.sofka.stepdefinitions.setup;
+
+import static co.com.sofka.util.Log4jValues.LOG4J_PROPERTIES_FILE_PATH;
+
+public class BaseResources {
+    private static final String BASE_URI = "https://reqres.in";
+    private static final String BASE_PATH = "/api";
+    protected static final String LOGIN_RESOURCE = "/login";
+    protected static final String DELETE_RESOURCE = "/users/2";
+    protected static final String POST_RESOURCE = "/users";
+    protected static final String LIST_RESOURCE = "/users?page=2";
+}
+
